d0e651f8a08d3b2d463505c6c78802d4f7edfb2f
dchamikara <dchamikara@iom.int>
2024-10-02 11:38:58 +0530

    Initial commit

---
+++ src/test/java/com/iom/upstream/UpstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iom.upstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
